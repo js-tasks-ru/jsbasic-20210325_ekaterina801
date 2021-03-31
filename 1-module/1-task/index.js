f9c0d6fd78c24df3391ea5b7a4023729a2fb75bd
Ekaterina Masiuta <ekaterina.masiuta@ab-soft.net>
2021-03-31 20:28:15 +0300

    Task 1, subtusk 1: get factorial of number

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  if(n === 0 || n === 1) return 1;
+  for( let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
+
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5); // 120
