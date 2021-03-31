57b1c77ab5fb60b76b1321fa2edb7b6ac84a7927
Ekaterina Masiuta <ekaterina.masiuta@ab-soft.net>
2021-03-31 19:18:22 +0300

    Task 1, subtusk 4: Check spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  let strNew = str.toLowerCase();
+  return (strNew.includes('1xbet') || strNew.includes('xxx')) ? true : false;
 }
+
+checkSpam('1XbeT now');
+checkSpam('free xxxxx');
+checkSpam('innocent rabbit');
