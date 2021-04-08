782380f7a172bfb7468d4bf566f925b608cd2551
Ekaterina Masiuta <ekaterina.masiuta@ab-soft.net>
2021-04-08 08:26:53 +0300

    Module 3: got array of the name, filtered array, set camelCase for str

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
-  // ваш код...
+  let newArr = str.split('-');
+  for(let i = 1; i <= newArr.length - 1; i++) {
+    newArr[i] = newArr[i].slice(0, 1).toUpperCase() + newArr[i].slice(1)
+  }
+  return newArr.join('');
 }
