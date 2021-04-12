5935061e4346145f3074d5854320fa7398ce9a04
Ekaterina Masiuta <ekaterina.masiuta@ab-soft.net>
2021-04-12 14:50:24 +0300

    Module 3: get str with name+salary, get max/min in array

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,19 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = '';
+  
+  let newArr = users.filter(item => {
+    if(item.age <= age) {
+      return item
+    }
+  }).map(item1 => {
+    return `${item1.name}, ${item1.balance}`
+  })
+  
+  for(let i = 0; i < newArr.length; i++) {
+      str += `${newArr[i]}`;
+      if (i !== newArr.length - 1) {
+        str += '\n'
+      }
+  }
+  return str;
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr1 = str.split(' ').join().split(',');;
+	let arr2 = [];
+
+  for (let i = 0; i <= arr1.length; i++) {
+    if(parseFloat(arr1[i])) {
+      arr2.push(parseFloat(arr1[i]));
+    }
+  }
+  return {
+  	min: Math.min.apply(null, arr2),
+    max: Math.max.apply(null, arr2)
+  }
 }
